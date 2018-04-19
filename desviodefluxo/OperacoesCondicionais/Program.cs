82001fcbf3e035fbaab75ed0a677c8681e046b1b
bruno silvia <bruno77silvaa@gmail.com>
2018-04-19 11:12:16 -0300

    Declaração de variaveis

--- desviodefluxo/OperacoesCondicionais/Program.cs
+++ desviodefluxo/OperacoesCondicionais/Program.cs
@@ -6,7 +6,17 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //Comentário in-line(em linha)
+           /*
+                Comentário in-block(em bloco)
+           */
+        //declaração da variável. Ela sera do tipo inteira
+        int operacao;
+
+        //declaração das variaveis n1 e n2. Elas serão tipo double.
+        double n1, n2;
+
+
         }
     }
 }
